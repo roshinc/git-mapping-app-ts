7d886463cd6e7af6dfdde452a1aec91b08fc97b0
Roshin C <idroshin2010@gmail.com>
2024-06-21 17:29:10 -0400

    Update crosswalk map generator to use workspace path instead of normalized path

--- src/generators/crosswalk-map-generator.ts
+++ src/generators/crosswalk-map-generator.ts
@@ -90,7 +90,7 @@ const processWorkspacePaths = (
                 if (!crosswalk[normalizedGitUrl]) {
                     crosswalk[normalizedGitUrl] = new Set<string>();
                 }
-                crosswalk[normalizedGitUrl].add(normalizedPath);
+                crosswalk[normalizedGitUrl].add(workspacePath);
                 logger.debug(`Mapped ${workspacePath} to ${normalizedGitUrl}`);
             }
         }
